4350e4976e9090b6d87e4a1e73ddc07d64f11265
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2020-12-07 09:54:23 +0100

    started update GoboQuery to use FieldService

--- src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/gobo/GoboQuery.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/gobo/GoboQuery.kt
@@ -2,18 +2,25 @@ package no.skatteetaten.aurora.gobo.graphql.gobo
 
 import com.expediagroup.graphql.spring.operations.Query
 import graphql.schema.DataFetchingEnvironment
+import no.skatteetaten.aurora.gobo.domain.FieldService
 import no.skatteetaten.aurora.gobo.graphql.GoboInstrumentation
 import no.skatteetaten.aurora.gobo.security.checkValidUserToken
 import org.springframework.stereotype.Component
 import java.time.Instant
 
 @Component
-class GoboQuery(private val goboInstrumentation: GoboInstrumentation) : Query {
+class GoboQuery(private val fieldService: FieldService) : Query {
 
     private val startTime = Instant.now()
 
-    fun gobo(dfe: DataFetchingEnvironment): Gobo {
+    fun gobo(fieldName: String?, dfe: DataFetchingEnvironment): Gobo {
         dfe.checkValidUserToken()
+
+        fieldName?.let {
+            fieldService.getFieldWithName(it)
+
+        } ?: fieldService.getAllFields()
+
 
         val fields = goboInstrumentation.fieldUsage.fields.map {
             val userFieldClientList = goboInstrumentation.fieldUsage.getFieldUsers(it.key)
